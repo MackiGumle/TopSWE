3ce991179348141b0de9d99918bec38ca3a64482
Gumle <masterik676@gmail.com>
2024-01-17 02:19:56 +0100

    Solved Power of Three

---
+++ Power of Three/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using Microsoft.VisualBasic;
+
+namespace TopSWE;
+
+
+class Program
+{
+    public class Solution
+    {
+        public bool IsPowerOfThree(int n)
+        {
+            if(n <= 0)
+                return false;
+
+            return (1162261467) % n == 0 ? true : false;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Solution solution = new();
+
+        Console.WriteLine(int.MaxValue % 3);
+        Console.WriteLine((int.MaxValue - 1) % 3);
+        Console.WriteLine(Math.Log(int.MaxValue - 1, 3));
+        Console.WriteLine(Math.Pow(3, 19)); // equals 1162261467
+
+        Console.WriteLine(solution.IsPowerOfThree(27));
+    }
+}
