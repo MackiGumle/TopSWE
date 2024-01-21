e935547a9b301e72328d53b7d4dabc33eff50dfc
Gumle <masterik676@gmail.com>
2024-01-21 22:09:19 +0100

    idk

---
+++ Number of 1 Bits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace TopSWE;
+
+
+class Program
+{
+    public class Solution
+    {
+        public int HammingWeight(uint n)
+        {
+            if (n == 0)
+                return 0;
+
+            int count = 0;
+
+            for (int i = 0; i < sizeof(uint) * 8; i++)
+                if ((1 & (n >> i)) == 1)
+                    count++;
+
+            return count;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Solution solution = new();
+        string[] strs = { "flower", "flow", "flight" };
+
+        Console.WriteLine(solution.HammingWeight(11));
+    }
+}
---
+++ Range Sum Query - Immutable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using Microsoft.VisualBasic;
+
+namespace TopSWE;
+
+
+class Program
+{
+    public class NumArray
+    {
+        public NumArray(int[] nums)
+        {
+
+        }
+
+        public int SumRange(int left, int right)
+        {
+            
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        NumArray obj = new();
+
+
+    }
+}
